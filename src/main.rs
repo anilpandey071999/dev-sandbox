a9cf40fb9fb37fc8cfe21cf8fca57263db4cf6a2
anilpandey071999 <anilpandey071999@gmail.com>
2025-09-21 19:44:27 +0530

    Add Tokio and Anyhow dependencies for async file ops

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::fs::{self as tokio_fs, DirEntry};
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let mut entries = tokio_fs::read_dir("fake-logs/app/").await?;
+    let mut files = Vec::new();
+    while let Some(entry) = entries.next_entry().await?{
+        let file_name = entry.file_name().to_string_lossy().into_owned();
+        println!("file {}", file_name);
+        files.push(file_name);
+    }
+    
+    Ok(())
 }

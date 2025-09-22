7e663b2e18bf591904a8ffb121f426f80a24d806
anilpandey071999 <anilpandey071999@gmail.com>
2025-09-22 16:43:50 +0530

    Add concurrent access via multiple threads

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
 use kv::KvStore;
 
 fn main() {
-    let mut kv_store: KvStore<_, _> = KvStore::new();
-    kv_store.set("key".to_string(), "value".to_string());
-    kv_store.get(&"key".to_string()).unwrap();
+    // let mut kv_store: KvStore<_, _> = KvStore::new();
+    // kv_store.set("key".to_string(), "value".to_string());
+    // kv_store.get(&"key".to_string()).unwrap();
+    let thread1 = std::thread::spawn(move ||{
+        let mut kv_store: KvStore<_, _> = KvStore::new();
+        for _ in 0..10 {
+            kv_store.set("key1".to_string(), "value1".to_string());
+        }
+    });
+    
+    let thread2 = std::thread::spawn(move ||{
+        let mut kv_store: KvStore<_, _> = KvStore::new();
+        for _ in 0..10 {
+            kv_store.set("key2".to_string(), "value2".to_string());
+        }
+    });
+    
+    thread1.join().unwrap();
+    thread2.join().unwrap();
 }

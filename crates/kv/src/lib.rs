4a639bc6c2f9bb4ab40f0b637814c093d5808625
anilpandey071999 <anilpandey071999@gmail.com>
2025-09-23 22:56:50 +0530

    Remove logging functionality from KvStore

--- crates/kv/src/lib.rs
+++ crates/kv/src/lib.rs
@@ -1,5 +1,8 @@
-use std::{collections::HashMap, fmt::{Debug, Display}, hash::Hash, time::{ SystemTime}};
-use log::{self, Logger};
+use std::{
+    collections::HashMap,
+    fmt::{Debug, Display},
+    hash::Hash,
+};
 /*
  * key: T
  * value: U
@@ -7,23 +10,20 @@ use log::{self, Logger};
  */
 pub struct KvStore<T: Eq + Hash + Debug + Display, U: Debug + Display> {
     pub kv_store: HashMap<T, U>,
-    logger: log::Logger,
 }
 
 impl<T: Eq + Hash + Debug + Display + Clone, U: Debug + Display + Clone> KvStore<T, U> {
     pub fn new() -> Self {
         Self {
             kv_store: HashMap::new(),
-            logger: Logger::new()
         }
     }
 
     pub fn set(&mut self, key: T, value: U) -> bool {
         self.kv_store.insert(key.clone(), value.clone());
-        self.logger.append(format!("{:?} key: {:?}, value: {:?} \n", SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap(), key, value));
         true
     }
-    
+
     pub fn get(&self, key: &T) -> Option<&U> {
         if let Some(value) = self.kv_store.get(key) {
             Some(value)

12e5eef644d4b8dc754b4cd22004a2c919a3e522
anilpandey071999 <anilpandey071999@gmail.com>
2025-09-23 22:57:26 +0530

    Implement async logging with channel communication

--- crates/log/src/lib.rs
+++ crates/log/src/lib.rs
@@ -1,25 +1,48 @@
-use std::{fs::{self, File}, io::Write};
+use std::{
+    fs::{self, File},
+    io::Write,
+    sync::mpsc::Receiver,
+};
 
 pub struct Logger {
-    pub log: String,
+    rx: Receiver<String>,
     file: File,
 }
 
 impl Logger {
-    pub fn new() -> Self {
+    pub fn new(rx: Receiver<String>) -> Self {
         let file = fs::OpenOptions::new()
             .append(true)
             .create(true)
             .open("./logs/1.log")
             .expect("Failed to open file for appending");
 
-        Self {
-            log: String::new(),
-            file,
-        }
+        Self { rx, file }
     }
 
     pub fn append(&mut self, log: String) -> () {
-        self.file.write_all(log.as_bytes()).expect("failed to append log!!")
+        self.file
+            .write_all(log.as_bytes())
+            .expect("failed to append ")
+    }
+    
+    pub fn run(&mut self) {
+        println!("String the log reciving...");
+        
+        while let Ok(message) = self.rx.recv() {
+            let log_entry = if message.ends_with("\n"){
+                message
+            }else{
+                format!("{}\n",message)
+            };
+            
+            if let Err(e) = self.file.write_all(log_entry.as_bytes()) {
+                eprintln!("Failed to write log: {}", e);
+            } else {
+                let _ = self.file.flush();
+            }
+        }
+        
+        println!("Logger stopped: channel closed.");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,59 @@
 use kv::KvStore;
+use log::Logger;
+use std::{
+    sync::{Arc, RwLock},
+    thread,
+};
 
 fn main() {
-    // let mut kv_store: KvStore<_, _> = KvStore::new();
-    // kv_store.set("key".to_string(), "value".to_string());
-    // kv_store.get(&"key".to_string()).unwrap();
-    let thread1 = std::thread::spawn(move ||{
-        let mut kv_store: KvStore<_, _> = KvStore::new();
+    let kv_store = Arc::new(RwLock::new(KvStore::new()));
+    let (tx, rx) = std::sync::mpsc::channel();
+    let mut logs = Logger::new(rx);
+
+    let logger_thread = thread::spawn(move || {
+        logs.run();
+    });
+
+    let kv_thread1 = kv_store.clone();
+    let tx_thread1 = tx.clone();
+    let thread1 = std::thread::spawn(move || {
         for _ in 0..10 {
-            kv_store.set("key1".to_string(), "value1".to_string());
+            // let a = kv_thread1.get_mut().unwrap();
+            if let Err(send_log_err) =
+                tx_thread1.send(format!("key: {} value: {}", "key1", "value1"))
+            {
+                eprintln!("Error sending log: {}", send_log_err);
+            }
+            kv_thread1
+                .write()
+                .unwrap()
+                .set("key1".to_string(), "value1".to_string());
+            // kv_store.set("key1".to_string(), "value1".to_string());
         }
     });
-    
-    let thread2 = std::thread::spawn(move ||{
-        let mut kv_store: KvStore<_, _> = KvStore::new();
+
+    let kv_thread2 = kv_store.clone();
+    let tx_thread2 = tx.clone();
+    let thread2 = std::thread::spawn(move || {
         for _ in 0..10 {
-            kv_store.set("key2".to_string(), "value2".to_string());
+            if let Err(send_log_err) =
+                tx_thread2.send(format!("key: {} value: {}", "key2", "value2"))
+            {
+                eprintln!("Error sending log: {}", send_log_err);
+            }
+            kv_thread2
+                .write()
+                .unwrap()
+                .set("key2".to_string(), "value2".to_string());
         }
     });
-    
+
     thread1.join().unwrap();
     thread2.join().unwrap();
+
+    drop(tx);
+
+    logger_thread.join().unwrap();
+
+    println!("We have reached to end!!");
 }

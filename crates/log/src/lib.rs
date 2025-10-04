7c586e35892d58a523560b03cc672cab099bae7b
anilpandey071999 <anilpandey071999@gmail.com>
2025-10-04 16:11:43 +0530

    bug(in-process): Add logging macro and refactor Logger implementation

--- crates/log/src/lib.rs
+++ crates/log/src/lib.rs
@@ -1,7 +1,7 @@
 use std::{
     fs::{self, File},
     io::{BufWriter, Write},
-    sync::mpsc::Receiver,
+    sync::{RwLock, mpsc::Receiver},
 };
 /*
  * `buffer writer` first keep every log in the memory buffer before writing to file this will make few system call
@@ -9,9 +9,10 @@ use std::{
 pub struct Logger {
     rx: Receiver<String>,
     buf_file_writer: BufWriter<File>,
-    current_size: u64,
-    max_size: u64,
 }
+
+pub static CURRENT_FILE_SIZE: RwLock<u64> = RwLock::new(0);
+pub const MAX_FILE_SIZE: u64 = 0;
 
 impl Logger {
     pub fn new(rx: Receiver<String>) -> Self {
@@ -33,16 +34,15 @@ impl Logger {
         Self {
             rx,
             buf_file_writer: buf_write,
-            current_size: 0,
-            max_size: 1_000_000_00,
+            // current_size: 0,
+            // max_size: 1_000_000_00,
         }
     }
 
     pub fn append(&mut self, log: String) -> () {
         let bytes_message = log.as_bytes();
-
-        if self.current_size >= self.max_size {
-            self.current_size = 0;
+        if *CURRENT_FILE_SIZE.read().unwrap() >= MAX_FILE_SIZE {
+            *CURRENT_FILE_SIZE.write().unwrap() = 0;
             let file_path = format!("./logs/{}.log", chrono::Utc::now());
             let file = fs::OpenOptions::new()
                 .append(true)
@@ -54,16 +54,40 @@ impl Logger {
         self.buf_file_writer
             .write_all(bytes_message)
             .expect("failed to append ");
-        self.current_size += bytes_message.len() as u64;
+        *CURRENT_FILE_SIZE.write().unwrap() += bytes_message.len() as u64;
     }
 
     pub fn run(&mut self) {
         println!("String the log reciving...");
 
         while let Ok(message) = self.rx.recv() {
-            self.append(message);
+            // self.append(message);
+            info!(message);
         }
 
         println!("Logger stopped: channel closed.");
     }
+}
+
+#[macro_export]
+macro_rules! info {
+    ($buffer: expr) => {
+        let bytes_message = stringify!($loger).as_bytes();
+        let path = module_path!();
+        if *crate::CURRENT_FILE_SIZE.read().unwrap() >= crate::MAX_FILE_SIZE {
+            *crate::CURRENT_FILE_SIZE.write().unwrap() = 0;
+            let file_path = format!("./logs/{}.log", chrono::Utc::now());
+            let file = std::fs::OpenOptions::new()
+                .append(true)
+                .create(true)
+                .open(file_path)
+                .expect("Failed to open file for appending");
+            $buffer.buf_file_writer = BufWriter::new(file);
+        }
+        buf_file_writer
+            .write_all(bytes_message)
+            .expect("failed to append ");
+        *crate::CURRENT_FILE_SIZE.write().unwrap() += bytes_message.len() as u64;
+        println!("Hello Macro ! {:?} => {}", stringify!($loger), $loger);
+    };
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use kv::KvHandle;
-use log::Logger;
+use log::{Logger, info};
+use std::io::BufWriter;
 use std::thread;
 
 fn main() {
@@ -11,15 +12,17 @@ fn main() {
         logger.run();
     });
 
+    // info!("from main");
+    // info!(25);
     let kv_store_thread1 = kv_handle.store.clone();
     let tx_thread1 = tx.clone();
 
     let kv_store_thread2 = kv_handle.store.clone();
     let tx_thread2 = tx.clone();
-    
 
     let thread_handler1 = thread::spawn(move || {
-        for i in 0..1_000_000 {
+        // for i in 0..1_000_000 {
+        for i in 0..10 {
             let key = format!("Key{i}");
             let value = format!("value{i}");
             let _ = tx_thread1
@@ -32,9 +35,11 @@ fn main() {
     });
 
     let thread_handler2 = thread::spawn(move || {
-        for i in 1_00_000..2_000_000 {
+        // for i in 1_00_000..2_000_000 {
+        for i in 10..20 {
             let key = format!("Key{i}");
             let value = format!("value{i}");
+
             let _ = tx_thread2
                 .send(format!(
                     "thread2 iter count {i} | Key : {key} Value: {value} \n"
@@ -57,24 +62,23 @@ fn main() {
 // fn main(){
 //     let count_rwlock = std::sync::Arc::new(std::sync::RwLock::new(0));
 //     let falge_rwlock = std::sync::Arc::new(std::sync::RwLock::new(true));
-    
+
 //     let thread_count1_rwlock = count_rwlock.clone();
 //     let thread_flage1_rwlock = falge_rwlock.clone();
-    
+
 //     let thread_count2_rwlock = count_rwlock.clone();
 //     let thread_flage2_rwlock = falge_rwlock.clone();
-    
+
 //     let thread1 = thread::spawn(move ||{
 //         *thread_flage1_rwlock.write().unwrap() = false;
 //         for i in 0..1_00_00_000_00{
 //             *thread_count1_rwlock.write().unwrap() += 1;
 //             // println!("thread1 increamenting {i}");
-//         } 
+//         }
 //         *thread_flage1_rwlock.write().unwrap() = true;
 //         println!("Thread count thread 1 {}", thread_count1_rwlock.read().unwrap());
 //     });
-    
-    
+
 //     let thread2 = thread::spawn(move ||{
 //         let mut count = 0;
 //         while !*thread_flage2_rwlock.read().unwrap(){
@@ -84,12 +88,10 @@ fn main() {
 //         for i in 0..1_00_00_000_00{
 //             // println!("thread2 decreamenting {i}");
 //             *thread_count2_rwlock.write().unwrap() -= 1;
-//         } 
+//         }
 //     });
 //     thread1.join().unwrap();
 //     thread2.join().unwrap();
-    
-    
-    
+
 //     println!("Thread count {}", count_rwlock.read().unwrap());
 // }

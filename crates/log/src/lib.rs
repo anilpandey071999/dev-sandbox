0ccea83a0d52e0fd7b6cabdfa41d867e6a524a6b
anilpandey071999 <anilpandey071999@gmail.com>
2025-10-05 18:49:16 +0530

    Refactor logging implementation and file rotation logic
    The commit improves the logger by: - Using proper datetime formatting
    for log files - Implementing proper file rotation when size limit is
    reached - Creating logs directory if it doesn't exist - Cleaning up
    unused code and comments - Simplifying the info! macro implementation

--- crates/log/src/lib.rs
+++ crates/log/src/lib.rs
@@ -1,24 +1,29 @@
+use chrono::Utc;
 use lazy_static::lazy_static;
 use std::{
     fs::{self, File},
     io::{BufWriter, Write},
     sync::{RwLock, mpsc::Receiver},
 };
+
 /*
  * `buffer writer` first keep every log in the memory buffer before writing to file this will make few system call
  */
 pub struct Logger {
     rx: Receiver<String>,
-    buf_file_writer: BufWriter<File>,
+    // _buf_file_writer: BufWriter<File>,
 }
 
 lazy_static! {
-    static ref file_name: String = format!("logs/{}", chrono::Utc::now());
-    static ref BUFFER_WRITER: RwLock<BufWriter<File>> =
-        RwLock::new(BufWriter::new(File::create(file_name.as_str()).unwrap()));
+    pub static ref BUFFER_WRITER: RwLock<BufWriter<File>> = {
+        if fs::read_dir("./logs").is_err() {
+            fs::create_dir("./logs").expect("Failed to create logs directory");
+        }
+        let file_name = format!("logs/{}.log", Utc::now().format("%Y-%m-%dT%H-%M-%S"));
+        let file = File::create(&file_name).expect("Failed to create log file");
+        RwLock::new(BufWriter::new(file))
+    };
 }
-
-// static mut BUFFER_WRITER: RwLock<Option<BufWriter<File>>> = RwLock::new(None);
 pub static CURRENT_FILE_SIZE: RwLock<u64> = RwLock::new(0);
 pub const MAX_FILE_SIZE: u64 = 1_000_000_000;
 
@@ -31,20 +36,7 @@ impl Logger {
                 panic!("Failed to crate logs folder {}", err);
             }
         }
-
-        let file_path = format!("./logs/{}.log", chrono::Utc::now());
-        let file = fs::OpenOptions::new()
-            .append(true)
-            .create(true)
-            .open(file_path)
-            .expect("Failed to open file for appending");
-        let buf_write = BufWriter::new(file);
-        Self {
-            rx,
-            buf_file_writer: buf_write,
-            // current_size: 0,
-            // max_size: 1_000_000_00,
-        }
+        Self { rx }
     }
 
     // pub fn append(&mut self, log: String) -> () {
@@ -67,14 +59,6 @@ impl Logger {
 
     pub fn run(&mut self) {
         println!("String the log reciving...");
-        // let file_path = format!("./logs/{}.log", chrono::Utc::now());
-        // let file = std::fs::OpenOptions::new()
-        //     .append(true)
-        //     .create(true)
-        //     .open(file_path)
-        //     .expect("Failed to open file for appending");
-        // let a = *BUFFER_WRITER.write().unwrap();
-        // a =
 
         while let Ok(message) = self.rx.recv() {
             info!(message);
@@ -84,41 +68,30 @@ impl Logger {
     }
 }
 
-// #[macro_export]
-// macro_rules! info {
-//     ($message: expr) => {
-//         // logger_writer!()
-//     };
-// }
-
 #[macro_export]
 macro_rules! info {
     ($message: expr) => {
-        use crate::CURRENT_FILE_SIZE;
-        // let buffer = Buf
+        use std::io::Write;
+        let mut writer = $crate::BUFFER_WRITER.write().unwrap();
+        let message = format!("{} {}", module_path!(), $message);
 
-        let mut buffer_writer = BUFFER_WRITER.write().unwrap();
-        println!(
-            "{} {}",
-            *crate::CURRENT_FILE_SIZE.read().unwrap(),
-            crate::MAX_FILE_SIZE
-        );
-        let bytes_message = format!("{} {}", module_path!(), $message);
-        // let path = module_path!();
-        if *crate::CURRENT_FILE_SIZE.read().unwrap() >= crate::MAX_FILE_SIZE {
-            *crate::CURRENT_FILE_SIZE.write().unwrap() = 0;
-            let file_path = format!("./logs/{}.log", chrono::Utc::now());
+        let mut size = $crate::CURRENT_FILE_SIZE.write().unwrap();
+        if *size >= $crate::MAX_FILE_SIZE {
+            *size = 0;
+            let file_name = format!(
+                "logs/{}.log",
+                chrono::Utc::now().format("%Y-%m-%dT%H-%M-%S")
+            );
             let file = std::fs::OpenOptions::new()
                 .append(true)
                 .create(true)
-                .open(file_path)
+                .open(&file_name)
                 .expect("Failed to open file for appending");
-            *buffer_writer = BufWriter::new(file);
+            *writer = std::io::BufWriter::new(file);
         }
-        buffer_writer
-            .write_all(bytes_message.as_bytes())
-            .expect("failed to append ");
-        *crate::CURRENT_FILE_SIZE.write().unwrap() += bytes_message.len() as u64;
-        println!("Hello Macro ! {:?} => {}", stringify!($loger), $message);
+        writer
+            .write_all(message.as_bytes())
+            .expect("failed to append");
+        *size += message.len() as u64;
     };
 }

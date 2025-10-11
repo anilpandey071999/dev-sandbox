39603713208a410adcb08cb329faa640138bc396
anilpandey071999 <anilpandey071999@gmail.com>
2025-10-11 10:51:35 +0530

    Initialize default Raft config with node ID 1

--- crates/node/src/lib.rs
+++ crates/node/src/lib.rs
@@ -3,5 +3,7 @@ use raft::prelude::*;
 
 #[tokio::main]
 async fn start_node() {
-    
+    let config = Config { id: 1, ..Default::default() } ;
+    // let store
+    // raft::RawNode::new(&config, store, logger)
 }

a2b035caf126e67bb9665b0b4ab092a75d4776e6
anilpandey071999 <anilpandey071999@gmail.com>
2025-10-17 11:18:40 +0530

    feat: add

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-// use std::io::{self, Write, stdin, stdout};
+use std::io::{self, Write, stdin, stdout};
+
+use std::sync::{Arc, Mutex};
+use std::{
+    collections::HashMap,
+    sync::{LazyLock, RwLock},
+};
 
 use crate::{
     documents::{Document, Documents},
@@ -7,6 +13,10 @@ use crate::{
 
 pub mod documents;
 pub mod search_engine;
+
+static GLOBLE_DATA_BASE: LazyLock<Mutex<HashMap<u64, (String, String)>>> =
+    LazyLock::new(|| Mutex::new(HashMap::new()));
+
 fn main() {
     println!("Hello, world!");
     let mut documents = Documents::new();
@@ -136,64 +146,186 @@ fn main() {
     // get_user_input()
 }
 
-// fn get_user_input() {
-//     let mut documents = Documents::new();
-//     let mut hash = SearchEngine::new();
-//     loop {
-//         print!(
-//             "> Choose the option \n> 1 for adding document \n> 2 for searching \n> 3 for seen all the documents \n> 4 for exiting \n>"
-//         );
-//         stdout().flush().unwrap();
+fn get_user_input() {
+    let mut documents = Documents::new();
+    let mut hash = SearchEngine::new();
+    loop {
+        print!(
+            "> Choose the option \n> 1 for adding document \n> 2 for searching \n> 3 for seen all the documents \n> 4 for exiting \n>"
+        );
+        stdout().flush().unwrap();
 
-//         let mut choose = String::new();
-//         stdin().read_line(&mut choose).unwrap();
-//         // if choose.contains("exits") {
-//         //     break;
-//         // }
-//         let mut title = String::new();
-//         let mut content = String::new();
-//         let choose = choose.trim().parse::<i32>().unwrap();
-//         match choose {
-//             1 => adding_document(&mut documents, &mut hash, &mut title, &mut content).unwrap(),
-//             2 => searching_doc(&mut documents, &mut hash),
-//             // 3 =>
-//             _ => println!("invalied choose"),
+        let mut choose = String::new();
+        stdin().read_line(&mut choose).unwrap();
+        // if choose.contains("exits") {
+        //     break;
+        // }
+        let choose = choose.trim().parse::<i32>().unwrap();
+        match choose {
+            1 => adding_document(&mut documents, &mut hash).unwrap(),
+            // 2 => searching_doc(&mut documents, &mut hash),
+            // 3 =>
+            _ => println!("invalied choose"),
+        }
+    }
+}
+
+fn adding_document<'a>(
+    documents: &'a mut Documents<'a>,
+    hash: &'a mut SearchEngine<'a>,
+) -> Result<(), String> {
+    println!("> Enter the title (or 'exit' to quit): ");
+    stdout().flush().unwrap();
+
+    let mut title = String::new();
+    let mut content = String::new();
+    stdin().read_line(&mut title).unwrap();
+    let title = title.trim().to_string();
+
+    if title.eq_ignore_ascii_case("exit") {
+        println!("> Exiting...");
+        return Err("Exiting...".to_string());
+    }
+
+    println!("Enter document content: ");
+    stdout().flush().unwrap();
+
+    stdin().read_line(&mut content).unwrap();
+    let content = content.trim().to_string();
+
+    let _ = GLOBLE_DATA_BASE.lock().unwrap().insert(1, (title, content));
+    // let get_value = GLOBLE_DATA_BASE.read().unwrap().get(&1).unwrap();
+    // let reader = GLOBLE_DATA_BASE.read().unwrap().clone();
+    // let gard = GLOBLE_DATA_BASE;
+    // let (tit, content) = *gard.get(&1).unwrap();
+    if let Some((title, content)) = GLOBLE_DATA_BASE.lock().unwrap().get(&1) {
+        // let a = Arc::new(tit)
+        documents.insert_documents(title, content, hash);
+    }
+    // documents.insert_documents(
+    //     &*GLOBLE_DATA_BASE.lock().unwrap().get(&1).unwrap().0,
+    //     &*GLOBLE_DATA_BASE.lock().unwrap().get(&1).unwrap().0,
+    //     hash,
+    // );
+    // }
+
+    Ok(())
+}
+
+fn searching_doc(documents: &mut Documents, hash: &mut SearchEngine) {
+    println!("yet to be implemented...");
+}
+
+// fn viewing_docs() {
+//     println!("{:?}",)
+// }
+
+
+// pub struct Node {
+//     pub data: i32,
+//     pub next: Option<Box<Node>>,
+// }
+// impl Node {
+//     pub fn new(data: i32) -> Self {
+//         Self { data, next: None }
+//     }
+// }
+
+// pub struct LinkList {
+//     pub head: Option<Box<Node>>,
+// }
+// impl LinkList {
+//     pub fn new(data: i32) -> Self {
+//         Self {
+//             head: Some(Box::new(Node::new(data))),
+//         }
+//     }
+
+//     pub fn delete_at_end(&mut self) {
+//         let mut current = &mut self.head;
+//         while let Some(node) = current {
+//             if !node.as_ref().next.is_none() && node.next.as_ref().unwrap().next.is_none() {
+//                 let deleted_node = node.next.take();
+//                 node.next = None;
+//                 println!("deleted node {}", deleted_node.unwrap().data);
+//                 return;
+//             }
+//             current = &mut node.next;
+//         }
+//     }
+
+//     pub fn push_at_end(&mut self, data: i32) {
+//         let new_node = Box::new(Node::new(data));
+//         let mut current = &mut self.head;
+//         while let Some(node) = current {
+//             if node.next.is_none() {
+//                 node.next = Some(new_node);
+//                 return;
+//             }
+//             current = &mut node.next;
+//         }
+//     }
+
+//     pub fn delete_at_frount(&mut self) {
+//         let deleted_node = self.head.take();
+//         println!("deleted node {}", deleted_node.as_ref().unwrap().data);
+//         self.head = deleted_node.unwrap().next;
+//     }
+
+//     pub fn push_at_frount(&mut self, data: i32) {
+//         let mut new_head = Box::new(Node::new(data));
+//         let head = self.head.take();
+//         new_head.next = head;
+//         self.head = Some(new_head);
+//     }
+
+//     pub fn push_at_pos(&mut self, data: i32, pos: u32) {
+//         let mut current = &mut self.head;
+//         let mut node_tracker = 1;
+//         while let Some(node) = current {
+//             if pos == node_tracker {
+//                 let take_data = node.next.take();
+//                 let new_node = Box::new(Node {
+//                     data,
+//                     next: take_data,
+//                 });
+//                 node.next = Some(new_node);
+//                 return;
+//             }
+//             node_tracker += 1;
+//             current = &mut node.next;
 //         }
 //     }
 // }
 
-// fn adding_document<'a>(
-//     documents: &'a mut Documents<'a>,
-//     hash: &'a mut SearchEngine<'a>,
-//     title: &'a mut String,
-//     content: &'a mut String,
-// ) -> Result<(), String> {
-//     println!("> Enter the title (or 'exit' to quit): ");
-//     stdout().flush().unwrap();
+// impl std::fmt::Display for LinkList {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         let mut current = &self.head;
+//         while let Some(node) = current {
+//             write!(f, r#"Node Data {} -> "#, node.data).unwrap();
+//             current = &node.next;
+//         }
+//         Ok(())
+//     }
+// }
 
-//     stdin().read_line(title).unwrap();
-//     let title = title.trim().to_string();
+// fn main() {
+//     let mut link_list = LinkList::new(0);
 
-//     if title.eq_ignore_ascii_case("exit") {
-//         println!("> Exiting...");
-//         return Err("Exiting...".to_string());
+//     for i in 0..10 {
+//         link_list.push_at_end(i);
 //     }
 
-//     println!("Enter document content: ");
-//     stdout().flush().unwrap();
+//     for i in 11..20 {
+//         link_list.push_at_frount(i);
+//     }
 
-//     stdin().read_line(content).unwrap();
-//     let content = content.trim().to_string();
+//     link_list.push_at_pos(100, 3);
+//     // link_list.push_at_end(10);
+//     println!("{}", link_list);
 
-//     documents.insert_documents(title.as_str(), content.as_str(), hash);
+//     link_list.delete_at_end();
 
-//     Ok(())
-// }
-
-// fn searching_doc(documents: &mut Documents, hash: &mut SearchEngine) {
-//     println!("yet to be implemented...");
-// }
-
-// fn viewing_docs() {
-//     println!("{:?}", )
+//     link_list.delete_at_frount();
+//     println!("{}", link_list);
 // }

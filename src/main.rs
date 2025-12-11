dc43640e0080fbf93cca33ad741159dc187cf68f
anilpandey071999 <anilpandey071999@gmail.com>
2025-12-11 11:40:54 +0530

    feat: Add actor model tests

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::os::macos::raw::stat;
-
+use std::sync::Arc;
 use tokio::sync::{
+    Mutex,
     mpsc::channel,
-    mpsc::{Receiver, Sender},
+    mpsc::Receiver,
     oneshot,
 };
 
@@ -11,7 +11,7 @@ struct Message {
     responder: oneshot::Sender<i64>,
 }
 
-async fn basic_actor(mut rx: Receiver<Message>) {
+async fn _basic_actor(mut rx: Receiver<Message>) {
     let mut state = 0;
 
     while let Some(msg) = rx.recv().await {
@@ -32,6 +32,12 @@ async fn resp_actor(mut rx: Receiver<Message>) {
     }
 }
 
+async fn _actor_replacement(state: Arc<Mutex<i64>>, value: i64) -> i64 {
+    let mut state = state.lock().await;
+    *state += value;
+    return *state;
+}
+
 #[tokio::main]
 async fn main() {
     let (tx, rx) = channel::<Message>(100);
@@ -47,5 +53,32 @@ async fn main() {
         };
         tx.send(msg).await.unwrap();
         println!("Respone: {}", resp_rx.await.unwrap());
+    }
+}
+
+#[cfg(test)]
+mod actor_model_tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_actor_replacement() {
+        let state = Arc::new(Mutex::new(0));
+        let mut handles = Vec::new();
+
+        let now = tokio::time::Instant::now();
+        // 0..1_000_000_00
+        for i in  0..100_000_000{
+            let state_ref = state.clone();
+            let future = async move {
+                let handle = tokio::spawn(async move { _actor_replacement(state_ref, i).await });
+                let _ = handle.await.unwrap();
+            };
+            handles.push(tokio::spawn(future));
+        }
+        for handle in handles {
+            let _ = handle.await.unwrap();
+        }
+
+        println!("Time elapsed: {:?}", now.elapsed());
     }
 }

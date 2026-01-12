bec30269ae521e92d0ec7c8a7614ec7d4de1137a
Anil <anilpandey071999@gmail.com>
2026-01-12 20:07:16 +0530

    feat: add queue for router and it's set get and delete method

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,7 @@ enum RoutingMessage {
     KeyValue(KeyValueMessage),
 }
 
+/// the main operation of the key-value actor
 async fn key_value_actor(mut receiver: Receiver<KeyValueMessage>) {
     let mut map: HashMap<String, Vec<u8>> = std::collections::HashMap::new();
     while let Some(message) = receiver.recv().await {
@@ -54,6 +55,7 @@ async fn key_value_actor(mut receiver: Receiver<KeyValueMessage>) {
     }
 }
 
+/// the decision of the router actor
 async fn router(mut receiver: Receiver<RoutingMessage>) {
     let (key_value_sender, key_value_receiver) = channel(32);
     tokio::spawn(key_value_actor(key_value_receiver));
@@ -67,4 +69,65 @@ async fn router(mut receiver: Receiver<RoutingMessage>) {
     }
 }
 
-fn main() {}
+/// the main queue
+static ROUTER_SENDER: OnceLock<Sender<RoutingMessage>> = OnceLock::new();
+
+pub async fn set(key: String, value: Vec<u8>) -> Result<(), std::io::Error> {
+    let (tx, rx) = oneshot::channel();
+    ROUTER_SENDER
+        .get()
+        .unwrap()
+        .send(RoutingMessage::KeyValue(KeyValueMessage::Set(
+            SetKeyValueMessage {
+                key,
+                value,
+                response: tx,
+            },
+        )))
+        .await
+        .unwrap();
+    rx.await.unwrap();
+    Ok(())
+}
+
+pub async fn get(key: String) -> Result<Option<Vec<u8>>, std::io::Error> {
+    let (tx, rx) = oneshot::channel();
+    ROUTER_SENDER
+        .get()
+        .unwrap()
+        .send(RoutingMessage::KeyValue(KeyValueMessage::Get(
+            GetKeyValueMessage { key, response: tx },
+        )))
+        .await
+        .unwrap();
+    Ok(rx.await.unwrap())
+}
+
+pub async fn delete(key: String) -> Result<(), std::io::Error> {
+    let (tx, rx) = oneshot::channel();
+    ROUTER_SENDER
+        .get()
+        .unwrap()
+        .send(RoutingMessage::KeyValue(KeyValueMessage::Delete(
+            DeleteKeyValueMessage { key, response: tx },
+        )))
+        .await
+        .unwrap();
+    rx.await.unwrap();
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let (sender, receiver) = channel(32);
+    ROUTER_SENDER.set(sender).unwrap();
+    tokio::spawn(router(receiver));
+
+    let _ = set("hello".to_string(), b"world".to_vec()).await.unwrap();
+    let value = get("hello".to_owned()).await?;
+    println!("value: {:?}", String::from_utf8(value.unwrap()));
+    let _ = delete("hello".to_owned()).await?;
+    let value = get("hello".to_owned()).await?;
+    println!("value: {:?}", value);
+    Ok(())
+}

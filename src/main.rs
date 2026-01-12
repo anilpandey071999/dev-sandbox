cc7e9e5eccee636f29f97460654cf91695003ddb
Anil <anilpandey071999@gmail.com>
2026-01-12 11:51:31 +0530

    feat: add router interface for set get delete for kv db

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,70 @@
-use std::sync::Arc;
+use std::{collections::HashMap, sync::OnceLock};
 use tokio::sync::{
-    Mutex,
     mpsc::channel,
-    mpsc::Receiver,
+    mpsc::{Receiver, Sender},
     oneshot,
 };
 
-struct Message {
-    value: i64,
-    responder: oneshot::Sender<i64>,
+struct SetKeyValueMessage {
+    key: String,
+    value: Vec<u8>,
+    response: oneshot::Sender<()>,
+}
+struct GetKeyValueMessage {
+    key: String,
+    response: oneshot::Sender<Option<Vec<u8>>>,
+}
+struct DeleteKeyValueMessage {
+    key: String,
+    response: oneshot::Sender<()>,
 }
 
-async fn _basic_actor(mut rx: Receiver<Message>) {
-    let mut state = 0;
-
-    while let Some(msg) = rx.recv().await {
-        state += msg.value;
-        println!("Received: {}", msg.value);
-        println!("State: {}", state);
-    }
+enum KeyValueMessage {
+    Get(GetKeyValueMessage),
+    Set(SetKeyValueMessage),
+    Delete(DeleteKeyValueMessage),
+}
+enum RoutingMessage {
+    KeyValue(KeyValueMessage),
 }
 
-async fn resp_actor(mut rx: Receiver<Message>) {
-    let mut state = 0;
-
-    while let Some(msg) = rx.recv().await {
-        state += msg.value;
-        if msg.responder.send(state).is_err() {
-            eprintln!("Failed to send response");
+async fn key_value_actor(mut receiver: Receiver<KeyValueMessage>) {
+    let mut map: HashMap<String, Vec<u8>> = std::collections::HashMap::new();
+    while let Some(message) = receiver.recv().await {
+        match message {
+            KeyValueMessage::Get(GetKeyValueMessage { key, response }) => {
+                let _ = response.send(map.get(&key).cloned());
+            }
+            KeyValueMessage::Delete(DeleteKeyValueMessage {
+                key,
+                response: responce,
+            }) => {
+                map.remove(&key);
+                let _ = responce.send(());
+            }
+            KeyValueMessage::Set(SetKeyValueMessage {
+                key,
+                value,
+                response,
+            }) => {
+                map.insert(key, value);
+                let _ = response.send(());
+            }
         }
     }
 }
 
-async fn _actor_replacement(state: Arc<Mutex<i64>>, value: i64) -> i64 {
-    let mut state = state.lock().await;
-    *state += value;
-    return *state;
-}
+async fn router(mut receiver: Receiver<RoutingMessage>) {
+    let (key_value_sender, key_value_receiver) = channel(32);
+    tokio::spawn(key_value_actor(key_value_receiver));
 
-#[tokio::main]
-async fn main() {
-    let (tx, rx) = channel::<Message>(100);
-
-    // let _actor_handle = tokio::spawn(basic_actor(rx));
-    let _resp_actor_handle = tokio::spawn(resp_actor(rx));
-
-    for i in 1..10 {
-        let (resp_tx, resp_rx) = oneshot::channel();
-        let msg = Message {
-            value: i,
-            responder: resp_tx,
-        };
-        tx.send(msg).await.unwrap();
-        println!("Respone: {}", resp_rx.await.unwrap());
+    while let Some(message) = receiver.recv().await {
+        match message {
+            RoutingMessage::KeyValue(message) => {
+                let _ = key_value_sender.send(message).await;
+            }
+        }
     }
 }
 
-#[cfg(test)]
-mod actor_model_tests {
-    use super::*;
-
-    #[tokio::test]
-    async fn test_actor_replacement() {
-        let state = Arc::new(Mutex::new(0));
-        let mut handles = Vec::new();
-
-        let now = tokio::time::Instant::now();
-        // 0..1_000_000_00
-        for i in  0..100_000_000{
-            let state_ref = state.clone();
-            let future = async move {
-                let handle = tokio::spawn(async move { _actor_replacement(state_ref, i).await });
-                let _ = handle.await.unwrap();
-            };
-            handles.push(tokio::spawn(future));
-        }
-        for handle in handles {
-            let _ = handle.await.unwrap();
-        }
-
-        println!("Time elapsed: {:?}", now.elapsed());
-    }
-}
+fn main() {}
